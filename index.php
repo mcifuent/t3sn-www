8737eaa6ed01a0204447bc83ebce29396f41dd12
cf <cifurello@gmail.com>
2017-11-05 15:29:16 +0100

    Signed-off-by: cf <cifurello@gmail.com>

--- index.php
+++ index.php
@@ -952,6 +952,7 @@ DisableRedirections=False\r\n';
 
                     $('#link_next').addClass("disabled");
 
+
                 }
             }
 
@@ -1347,6 +1348,21 @@ window.location.href = mytext; */
 
         }); //Document.ready
     </script>
+	
+	<script type="text/javascript">
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//192.168.1.101/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+
 </head>
 
 <body style="margin:0px; font-size:18px;">
